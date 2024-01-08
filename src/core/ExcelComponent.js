87a00dd130beffd5f8161d8de670f64d6acf2e9a
magalutfullaev <mhoja9494@gmail.com>
2024-01-08 17:45:52 +0500

    add: Basic Framework

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,5 @@
+import {DomListener} from '@core/DomListener';
+
+export class ExcelComponent extends DomListener {
+
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import './module'
-import './scss/index.scss'
+import './scss/index.scss';
+import {Excel} from '@/components/excel/Excel';
+import {Toolbar} from '@/components/toolbar/Toolbar';
+import {Formula} from '@/components/formula/Formula';
+import {Header} from '@/components/header/Header';
+import {Table} from '@/components/table/Table';
 
-console.log(true)
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+});
+
+excel.render();
